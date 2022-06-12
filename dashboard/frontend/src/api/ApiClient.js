984315b73bce50478fb59517dbfb1d3cc05dd8cd
Daniel Vršek <daniel.vrsek@gmail.com>
2022-06-13 00:18:12 +0200

    remove unused components

--- dashboard/frontend/src/api/ApiClient.js
+++ dashboard/frontend/src/api/ApiClient.js
@@ -1,16 +1,14 @@
 import axios from 'axios';
 import { getBasePath } from '../components/utils/pathHelper';
 
-const getMethod = (path, headers) =>
-    axios.get(getBasePath() + path, { headers, withCredentials: true });
-const postMethod = (path, payload, headers) =>
+const getMethod = (path, headers?) => axios.get(getBasePath() + path, { headers, withCredentials: true });
+const postMethod = (path, payload, headers?) =>
     axios.post(getBasePath() + path, payload, {
         headers,
         withCredentials: true,
     });
-const deleteMethod = (path, headers) =>
-    axios.delete(getBasePath() + path, { headers, withCredentials: true });
-const putMethod = (path, payload, headers) =>
+const deleteMethod = (path, headers?) => axios.delete(getBasePath() + path, { headers, withCredentials: true });
+const putMethod = (path, payload, headers?) =>
     axios.put(getBasePath() + path, payload, {
         headers,
         withCredentials: true,
@@ -24,8 +22,7 @@ const ApiClient = {
     getUserInfo: async () => getMethod('/auth/user-info'),
     getWorkspaceInfo: async () => getMethod('/auth/workspace-info'),
     getUserAvailableWorkspaces: async () => getMethod('/workspaces/user'),
-    setUserWorkspace: async (workspaceId) =>
-        putMethod('/workspaces/user/current', { workspaceId }, getHeaders()),
+    setUserWorkspace: async (workspaceId) => putMethod('/workspaces/user/current', { workspaceId }, getHeaders()),
     login: async (credentials) => postMethod('/auth/login', credentials),
     register: (payload) => postMethod('/auth/register', payload),
     logout: () => postMethod('/auth/logout'),
@@ -36,16 +33,11 @@ const ApiClient = {
     createGateway: async (name) => postMethod('/gateways', { name }),
     getGateway: async (id) => getMethod(`/gateways/${id}`),
     getGateways: async () => getMethod(`/gateways`),
-    removeGatewayFromWokspace: async (gatewayId) =>
-        deleteMethod(`/gateways/${gatewayId}/workspace`),
+    removeGatewayFromWokspace: async (gatewayId) => deleteMethod(`/gateways/${gatewayId}/workspace`),
     getCurrentWorkspace: async () => getMethod(`/workspaces/user/current`),
-    getCurrentWorkspaceUsers: async () =>
-        getMethod(`/workspaces/current/users`),
-    addUserToCurrentWorkspace: async (username) =>
-        postMethod(`/workspaces/current/users`, { username }),
-    removeUserFromCurrentWokspace: async (userId) =>
-        deleteMethod(`/workspaces/current/users/${userId}`),
-    registerUser: async (payload) => postMethod('/auth/register', payload),
+    getCurrentWorkspaceUsers: async () => getMethod(`/workspaces/current/users`),
+    addUserToCurrentWorkspace: async (username) => postMethod(`/workspaces/current/users`, { username }),
+    removeUserFromCurrentWokspace: async (userId) => deleteMethod(`/workspaces/current/users/${userId}`),
 };
 
 export default ApiClient;
